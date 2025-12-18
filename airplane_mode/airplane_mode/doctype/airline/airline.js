a139c49836bb03fa7d6a5ded2fb12cac8a6cea80
manyam0258 <manyam.surendhranath@gmail.com>
2025-12-18 11:59:18 +0000

    added show-me and website in airline

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2025, surendhranath and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airline", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Airline', {
+    refresh: function (frm) {
+        if (frm.doc.website) {
+            frm.add_web_link(frm.doc.website, 'Visit Website');
+        }
+    }
+});
