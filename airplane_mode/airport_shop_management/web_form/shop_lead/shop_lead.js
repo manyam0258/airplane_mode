50a5f3ea9ccda968843639a5a5afd7a60f6ab4d8
manyam0258 <manyam.surendhranath@gmail.com>
2025-12-27 18:05:58 +0000

    refined webform for shops and shop lead

--- airplane_mode/airport_shop_management/web_form/shop_lead/shop_lead.js
+++ airplane_mode/airport_shop_management/web_form/shop_lead/shop_lead.js
@@ -1,3 +1,5 @@
-frappe.ready(function () {
-	// bind events here
-})
+frappe.web_form.after_load = function () {
+	if (frappe.route_options && frappe.route_options.shop) {
+		frappe.web_form.set_value("shop", frappe.route_options.shop);
+	}
+};
